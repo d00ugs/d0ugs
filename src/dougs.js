f9c85457fab34e6d10367fc992e2f81128081f86
Dougal Kennedy <Dougal.Kennedy@qnrl.com>
2018-09-20 16:52:26 +0100

    centered

--- src/dougs.js
+++ src/dougs.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import {Card, CardImg, Col, CardBody, CardText} from 'reactstrap';
+import {Container, Card, CardImg, Row, Col, CardBody, CardText} from 'reactstrap';
 import me from './me.jpg';
 
 class Dougs extends Component {
   render() {
     return (
-        <Col xs="3">
-            <Card>
-                <CardImg top width="30%" src={me}/>
-                <CardBody>
-                <CardText>dou.gs</CardText>
-                </CardBody>
-            </Card>
-        </Col>
+        <Container>
+            <Row>
+                <Col/>
+                <Col xs="3">
+                    <Card>
+                        <CardImg top width="100%" src={me}/>
+                        <CardBody>
+                        <CardText>dou.gs</CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col/>
+            </Row>
+        </Container>
     );
   }
 }
